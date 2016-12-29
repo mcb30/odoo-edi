7ce8abe959e29cc1fc12d87bf86db94d513184ee
Michael Brown <mbrown@fensystems.co.uk>
2016-12-29 17:19:23 +0000

    edi: Add E-mail Gateway for sending EDI documents via e-mail
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

---
+++ addons/edi/models/edi_connection_mail.py
@@ -0,0 +1,89 @@
+from odoo import api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
+from odoo.tools import generate_tracking_message_id
+from datetime import datetime, timedelta
+import os.path
+import fnmatch
+import base64
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class DummyConnection(object):
+    """Dummy connection object representing connection to mail server"""
+    def close(self):
+        pass
+
+
+class EdiConnectionMail(models.AbstractModel):
+    """EDI Mail connection
+
+    An EDI Mail connection is used to send EDI documents by e-mail.
+    """
+
+    _name = 'edi.connection.mail'
+    _description = 'EDI Mail Connection'
+
+    @api.model
+    def connect(self, gateway):
+        """Connect to mail server"""
+        return DummyConnection()
+
+    @api.model
+    def receive_inputs(self, conn, path, transfer):
+        """Receive input attachments"""
+        pass
+
+    @api.model
+    def send_outputs(self, conn, path, transfer):
+        """Send output attachments"""
+        Attachment = self.env['ir.attachment']
+        Document = self.env['edi.document']
+        Mail = self.env['mail.mail']
+
+        # Get message template
+        template = self.env.ref('edi.mail_template')
+
+        # Get list of output documents
+        min_date = (datetime.now() - timedelta(hours=path.age_window))
+        docs = Document.search([
+            ('execute_date', '>=', fields.Datetime.to_string(min_date)),
+            ('doc_type_id', 'in', path.doc_type_ids.mapped('id')),
+            ('output_ids', '!=', False),
+            ])
+
+        # Identify attachments already sent via this path
+        sent = Mail.search([
+            ('date', '>=', fields.Datetime.to_string(min_date)),
+            ('model', '=', 'edi.gateway.path'),
+            ('res_id', '=', path.id),
+            ]).mapped('attachment_ids')
+
+        # Send documents
+        outputs = Attachment.browse()
+        for doc in docs:
+
+            # Identify applicable attachments
+            attachments = doc.output_ids.filtered(
+                lambda x: fnmatch.fnmatch(x.datas_fname, path.glob)
+                )
+
+            # Skip documents where all attachments have already been sent
+            if attachments <= sent:
+                continue
+            outputs += attachments
+
+            # Create e-mail
+            vals = template.generate_email(doc.id)
+            vals['model'] = 'edi.gateway.path'
+            vals['res_id'] = path.id
+            vals['email_to'] = path.path
+            vals['attachment_ids'] = [(6, 0, [x.id for x in attachments])]
+            mail = Mail.create(vals)
+
+            # Send e-mail
+            mail.send(raise_exception=True)
+
+        return outputs
