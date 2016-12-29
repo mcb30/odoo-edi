1e2e50a23a64e7a9a2012c99104ad0689b7195e7
Michael Brown <mbrown@fensystems.co.uk>
2016-12-29 17:19:23 +0000

    edi: Add E-mail Gateway for sending EDI documents via e-mail
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@ import edi_attachment_audit
 import edi_issues
 import edi_document
 import edi_gateway
+import edi_connection_mail
 import edi_connection_sftp
 import edi_connection_xmlrpc
 import edi_record
