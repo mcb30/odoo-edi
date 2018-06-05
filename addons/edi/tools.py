5cc276c96094be6f551facbb5f589e52c9053792
Michael Brown <mbrown@fensystems.co.uk>
2018-06-05 23:40:03 +0100

    edi: Add batched() helper function
    A common requirement when processing EDI documents is to process
    records in batches, to obtain a sensible trade-off between the number
    of database queries and the size of each query.
    
    Add a batched() helper function which takes an iterable and constructs
    subsequences of a specified size, along with a range object to allow
    for easy progress logging by the caller.
    
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

---
+++ addons/edi/tools.py
@@ -0,0 +1,13 @@
+"""Helper tools for EDI"""
+
+from itertools import count, zip_longest
+
+def batched(iterable, size=1):
+    """Iterate over iterable in batches of a specified size"""
+    class Filler(object):
+        pass
+    iterator = iter(iterable)
+    batches = (list(x for x in batch if x is not Filler)
+               for batch in zip_longest(*[iterator] * size, fillvalue=Filler))
+    for start, batch in zip(count(step=size), batches):
+        yield (range(start, start + len(batch)), batch)
