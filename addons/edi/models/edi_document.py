f52cd3c58982a9de14a744e464f43ea98f29e804
Michael Brown <mbrown@fensystems.co.uk>
2018-06-06 11:20:50 +0100

    edi: Calculate record type name list only on demand
    Commit 3661865 ("edi: Expose document's record type names to view
    rendering logic") created the "rec_type_names" field used by
    edi.document views to determine the applicable EDI record notebook
    pages to be displayed for an EDI document.
    
    This commit treated "rec_types_names" as a stored field for
    edi.document.type and a related field for edi.document.  This leads to
    the field having an empty value when "doc_type_id" has not yet been
    set (e.g. when manually creating an EDI document).  This breaks the
    JavaScript domain evaluator, which attempts to use .indexOf() on an
    empty (i.e. false) value.
    
    As an interesting side note, the JavaScript domain evaluator appears
    to invert the "like" and "ilike" operators; fortunately, model names
    are always in lower case anyway.
    
    Fix by removing the stored field on edi.document.type and treating
    "rec_type_names" as a non-stored computed field directly on
    edi.document.
    
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- addons/edi/models/edi_document.py
+++ addons/edi/models/edi_document.py
@@ -51,8 +51,6 @@ class EdiDocumentType(models.Model):
                                domain=[('is_edi_document', '=', True)],
                                required=True, index=True)
     rec_type_ids = fields.Many2many('edi.record.type', string="Record Types")
-    rec_type_names = fields.Char(string="Record Type Names",
-                                 compute='_compute_rec_type_names', store=True)
 
     # Autodetection order when detecting a document type based upon
     # the set of input attachments.
@@ -69,28 +67,6 @@ class EdiDocumentType(models.Model):
 
     _sql_constraints = [('model_uniq', 'unique (model_id)',
                          "The document model must be unique")]
-
-    @api.multi
-    @api.depends('rec_type_ids', 'rec_type_ids.model_id',
-                 'rec_type_ids.model_id.model')
-    def _compute_rec_type_names(self):
-        """Compute record type name list
-
-        The record type name list is used by the view definitions to
-        determine whether or not to display particular record-specific
-        pages within the document form view.
-
-        This avoids the need for each record type to define a custom
-        boolean field on ``edi.document.type`` to convey the same
-        information.
-
-        Note that this hack would be entirely unnecessary if the Odoo
-        domain syntax allowed us to express the concept of "visible if
-        ``rec_type_ids`` contains <value>".
-        """
-        for doc_type in self:
-            rec_models = doc_type.mapped('rec_type_ids.model_id.model')
-            doc_type.rec_type_names = '/%s/' % '/'.join(rec_models)
 
     @api.model
     def autocreate(self, inputs):
@@ -186,8 +162,9 @@ class EdiDocument(models.Model):
     project_id = fields.Many2one(related='doc_type_id.project_id')
     issue_ids = fields.One2many(inverse_name='edi_doc_id')
 
-    # Related fields provided solely for use by views
-    rec_type_names = fields.Char(related='doc_type_id.rec_type_names')
+    # Record type names (solely for use by views)
+    rec_type_names = fields.Char(string="Record Type Names",
+                                 compute='_compute_rec_type_names')
 
     @api.depends('input_ids', 'input_ids.res_id')
     def _compute_input_count(self):
@@ -200,6 +177,29 @@ class EdiDocument(models.Model):
         """Compute number of output attachments (for UI display)"""
         for doc in self:
             doc.output_count = len(doc.output_ids)
+
+    @api.multi
+    @api.depends('doc_type_id', 'doc_type_id.rec_type_ids',
+                 'doc_type_id.rec_type_ids.model_id',
+                 'doc_type_id.rec_type_ids.model_id.model')
+    def _compute_rec_type_names(self):
+        """Compute record type name list
+
+        The record type name list is used by the view definitions to
+        determine whether or not to display particular record-specific
+        pages within the document form view.
+
+        This avoids the need for each record type to define a custom
+        boolean field on ``edi.document.type`` to convey the same
+        information.
+
+        Note that this hack would be entirely unnecessary if the Odoo
+        domain syntax allowed us to express the concept of "visible if
+        ``rec_type_ids`` contains <value>".
+        """
+        for doc in self:
+            rec_models = doc.mapped('doc_type_id.rec_type_ids.model_id.model')
+            doc.rec_type_names = '/%s/' % '/'.join(rec_models)
 
     @api.multi
     def _get_state_name(self):
