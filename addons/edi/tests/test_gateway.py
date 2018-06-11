e6f7fdef8e689c0efa99cb8831e3d57d08409dad
Miquel PS <miquel.palahi.sitges@unipart.io>
2018-06-11 13:35:21 +0100

    [UPD] edi: remove gateway as class variable

--- addons/edi/tests/test_gateway.py
+++ addons/edi/tests/test_gateway.py
@@ -13,38 +13,36 @@ class TestEDIGateway(common.BaseEDI):
     def setUp(self):
         super(TestEDIGateway, self).setUp()
 
-    def _test_action_test(self):
+    def _test_action_test(self, gateway):
         """ Test action test to check connection is fine """
 
-        old_messages = self.gateway.message_ids
-
-        self.gateway.action_test()
-        new_message = self.gateway.message_ids - old_messages
+        old_messages = gateway.message_ids
+        gateway.action_test()
+        new_message = gateway.message_ids - old_messages
         self.assertEqual(len(new_message), 1)
         # self.assertIsInstantiated(new_message)
         self.assertIn("Connection tested successfully", new_message.body)
 
-    def _test_do_transfer_no_path(self):
+    def _test_do_transfer_no_path(self, gateway):
         """ Test do transfer with no path, which means a transfer
             is created but nothing received
         """
 
-        old_transfers = self.gateway.transfer_ids
-
-        transfer = self.gateway.do_transfer()
-        new_transfer = self.gateway.transfer_ids - old_transfers
+        old_transfers = gateway.transfer_ids
+        transfer = gateway.do_transfer()
+        new_transfer = gateway.transfer_ids - old_transfers
         self.assertEqual(new_transfer, transfer)
         self.assertEqual(len(transfer.issue_ids), 0)
 
-    def _test_do_transfer_receive(self):
+    def _test_do_transfer_receive(self, gateway):
         """ Test do transfer with input path, which means a transfer
             is created and a file is received
         """
 
         # first transfer receives dummy file
-        old_transfers = self.gateway.transfer_ids
-        transfer = self.gateway.do_transfer()
-        new_transfer = self.gateway.transfer_ids - old_transfers
+        old_transfers = gateway.transfer_ids
+        transfer = gateway.do_transfer()
+        new_transfer = gateway.transfer_ids - old_transfers
         self.assertEqual(new_transfer, transfer)
         issue = transfer.issue_ids
         self.assertEqual(len(issue), 1)
@@ -52,24 +50,24 @@ class TestEDIGateway(common.BaseEDI):
         self.assertEqual(len(transfer.input_ids), 1)
 
         # second transfer ignores dummy file
-        old_transfers = self.gateway.transfer_ids
-        transfer = self.gateway.do_transfer()
-        new_transfer = self.gateway.transfer_ids - old_transfers
+        old_transfers = gateway.transfer_ids
+        transfer = gateway.do_transfer()
+        new_transfer = gateway.transfer_ids - old_transfers
         self.assertEqual(new_transfer, transfer)
         self.assertEqual(len(transfer.input_ids), 0)
 
-    def _test_do_transfer_receive_old_file_ignored(self):
+    def _test_do_transfer_receive_old_file_ignored(self, gateway):
         """ Test do transfer with input path to an old file,
             which means a transfer is created but nothing received
         """
 
-        old_transfers = self.gateway.transfer_ids
-        transfer = self.gateway.do_transfer()
-        new_transfer = self.gateway.transfer_ids - old_transfers
+        old_transfers = gateway.transfer_ids
+        transfer = gateway.do_transfer()
+        new_transfer = gateway.transfer_ids - old_transfers
         self.assertEqual(new_transfer, transfer)
         self.assertEqual(len(transfer.input_ids), 0)
 
-    def _test_do_transfer_send(self):
+    def _test_do_transfer_send(self, gateway):
         """ Test do transfer with output path """
 
         # create document in state completed
@@ -86,42 +84,37 @@ class TestEDILocalGateway(TestEDIGateway):
         super(TestEDILocalGateway, self).setUp()
 
     def test01_gateway_action_test(self):
-        self.gateway = self._create_local_gateway()
-        self._create_local_path(gateway=self.gateway,
+
+        gateway = self._create_local_gateway()
+        self._create_local_path(gateway=gateway,
                                 allow_send=False)
-        self._test_action_test()
+        self._test_action_test(gateway)
 
     def test02_gateway_do_transfer_no_path(self):
-        """ Create local gateway """
 
-        self.gateway = self._create_local_gateway()
-
-        self._test_do_transfer_no_path()
+        gateway = self._create_local_gateway()
+        self._test_do_transfer_no_path(gateway)
 
     def test03_gateway_do_transfer_receive(self):
-        """ Create local gateway """
 
-        self.gateway = self._create_local_gateway()
-        self._create_local_path(gateway=self.gateway,
+        gateway = self._create_local_gateway()
+        self._create_local_path(gateway=gateway,
                                 allow_send=False)
-        self._test_do_transfer_receive()
+        self._test_do_transfer_receive(gateway)
 
     def test04_gateway_do_transfer_receive_old_file_ignored(self):
 
-        self.gateway = self._create_local_gateway()
-        self._create_local_path(gateway=self.gateway,
+        gateway = self._create_local_gateway()
+        self._create_local_path(gateway=gateway,
                                 touch_files=False,
                                 folder='files/old',
                                 age_window=0.01)
-
-
-        self._test_do_transfer_receive_old_file_ignored()
+        self._test_do_transfer_receive_old_file_ignored(gateway)
 
     def test05_gateway_do_transfer_send(self):
 
-        self.gateway = self._create_local_gateway()
-        self._create_local_path(gateway=self.gateway,
+        gateway = self._create_local_gateway()
+        self._create_local_path(gateway=gateway,
                                 folder='files/out',
                                 allow_receive=False)
-
-        self._test_do_transfer_send()
+        self._test_do_transfer_send(gateway)
