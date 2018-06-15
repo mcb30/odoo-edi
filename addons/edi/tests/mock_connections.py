dca954d41ca2ddcfc2ee8396f4930fa33465b0a5
nocturnalastro <michele.costa@unipart.io>
2018-06-15 18:37:32 +0100

    [IMP] edi: Allow connection patch to be done via a context manager
    Less boiler plate for patching sftp connection while
    still allowing modification per test

--- addons/edi/tests/mock_connections.py
+++ addons/edi/tests/mock_connections.py
@@ -1,6 +1,7 @@
 import os
 from time import time
 from collections import namedtuple
+from unittest.mock import patch
 
 
 class DummySftpConnection(object):
@@ -45,13 +46,30 @@ class DummySftpConnection(object):
         else:
             return open(os.path.join(self.directory, filename), mode)
 
+    def rename(self, src_filepath, dst_filepath):
+        """Rename file"""
+        src_path, src_filename = os.path.split(src_filepath)
+        dst_path, dst_filename = os.path.split(dst_filepath)
+        self.actual[src_path].remove(src_filename)
+        self.actual[dst_path].append(dst_filename)
+        os.rename(os.path.join(self.output_directory, src_filename),
+                  os.path.join(self.output_directory, dst_filename))
+
     def close(self):
         return True
 
+    def get_channel(self):
+        class chan(object):
+            @staticmethod
+            def settimeout(*args, **kwargs):
+                pass
+        return chan
+
 
 class UdesDummySftpConnection(DummySftpConnection):
-    def __init__(self, inputs=None, outputs=None,
-                 in_folder='files', out_folder='out'):
+
+    def __init__(self, test_class,  inputs=None, outputs=None,
+                 in_folder='files', out_folder='out', side_effect=None):
         super(UdesDummySftpConnection, self).__init__(inputs, outputs)
         # override directory value to have the files for testing in the
         # current module
@@ -59,3 +77,25 @@ class UdesDummySftpConnection(DummySftpConnection):
             os.path.dirname(os.path.abspath(__file__)), in_folder
         )
         self.output_directory = os.path.join(self.directory, out_folder)
+        self._test_class = test_class
+        self._side_effect = side_effect
+
+    def __enter__(self, *args, **kwargs):
+        patch_sftp = patch('odoo.addons.edi.models.edi_connection_sftp.'
+                           'SFTPOnlyClient.from_transport',
+                           autospec=True,
+                           return_value=self,
+                           side_effect=self._side_effect)
+
+        patch_ssh = patch('paramiko.SSHClient.connect', autospec=True)
+
+        self._test_class.patched_sftp = patch_sftp.start()
+        self._test_class.addCleanup(patch_sftp.stop)
+        self._test_class.patched_ssh = patch_ssh.start()
+        self._test_class.addCleanup(patch_ssh.stop)
+
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self._test_class.doCleanups()
+        return False
