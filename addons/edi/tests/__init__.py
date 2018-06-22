bcd7f92b4614b1e0f9ecb94d4dccc998c6ebfda8
Miquel PS <miquel.palahi.sitges@unipart.io>
2018-06-22 09:38:45 +0100

    [TEST] edi: Add edi.connection.xmlrpc unit tests file
    Signed-off-by: Miquel PS <miquel.palahi.sitges@unipart.io>

--- addons/edi/tests/__init__.py
+++ addons/edi/tests/__init__.py
@@ -2,6 +2,7 @@
 
 from . import test_edi_connection_local
 from . import test_edi_connection_mail
+from . import test_edi_connection_xmlrpc
 from . import test_edi_document
 from . import test_edi_issue
 from . import test_edi_transfer
