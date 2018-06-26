587addf11e2e91c1a261a912b5f6b1a14bd8e11f
Michael Brown <mbrown@fensystems.co.uk>
2018-06-26 17:05:48 +0100

    [TEST] edi: Extend sapidoc.model unit test coverage to 100%
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- addons/edi/tests/test_sap.py
+++ addons/edi/tests/test_sap.py
@@ -2,6 +2,7 @@
 
 import base64
 from ..tools import sap_idoc_type, SapIDoc
+from ..tools.sapidoc.model import CharacterField
 from .common import EdiCase
 
 
@@ -23,6 +24,7 @@ class TestSapIDoc(EdiCase):
         """Test IDoc parsing"""
         Matmas01 = SapIDoc('edi', 'tests', 'files', 'matmas01.txt')
         idoc = Matmas01(base64.b64decode(self.chocolate.datas))
+        self.assertIsInstance(idoc.data[0].__class__.SEGNAM, CharacterField)
         self.assertEqual(idoc.control.DOCNUM, "0000000000198012")
         self.assertEqual(len(idoc.data), 4)
         self.assertEqual(idoc.data[0].SEGNAM, "E2MARAM009")
@@ -30,3 +32,11 @@ class TestSapIDoc(EdiCase):
         self.assertEqual(idoc.data[1].MAKTX, "Chocolate chunk cookie mix")
         self.assertEqual(idoc.data[2].GEWEI, "GRM")
         self.assertEqual(idoc.data[3].EAN11, "5010251522676")
+        self.assertEqual(idoc.data[3].EANTP, "HE") # next field in line
+        idoc.data[3].EAN11 = "5055365625417"
+        self.assertEqual(idoc.data[3].EAN11, "5055365625417")
+        self.assertEqual(idoc.data[3].EANTP, "HE") # next field in line
+        self.assertIn("EAN11='5055365625417'", repr(idoc.data[3]))
+        self.assertIn("EAN11='5055365625417'", repr(idoc))
+        self.assertIn("DOCNUM='0000000000198012'", repr(idoc))
+        self.assertIn("DOCNUM='0000000000198012'", str(idoc))
