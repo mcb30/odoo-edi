d38d82e8e416ba585964c24777dd14583ce3e55e
Michael Brown <mbrown@fensystems.co.uk>
2018-06-27 01:39:21 +0100

    [TEST] edi_product: Add basic unit tests for product tutorial document
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

---
+++ addons/edi_product/tests/test_tutorial.py
@@ -0,0 +1,54 @@
+"""EDI product tutorial tests"""
+
+from .common import EdiProductCase
+
+
+class TestTutorial(EdiProductCase):
+    """EDI product tutorial tests"""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.doc_type_tutorial = cls.env.ref(
+            'edi_product.tutorial_document_type'
+        )
+
+    @classmethod
+    def create_tutorial(cls, filename):
+        """Create product tutorial document"""
+        EdiDocument = cls.env['edi.document']
+        doc = EdiDocument.create({
+            'name': "Tutorial test",
+            'doc_type_id': cls.doc_type_tutorial.id,
+        })
+        cls.create_input_attachment(doc, filename)
+        return doc
+
+    def test01_basic(self):
+        """Basic document execution"""
+        doc = self.create_tutorial('books01.csv')
+        self.assertTrue(doc.action_execute())
+        products = doc.mapped('product_tutorial_ids.product_id')
+        self.assertEqual(len(products), 3)
+        products_by_code = {x.default_code: x for x in products}
+        self.assertEqual(products_by_code['9780552145428'].name, "Hogfather")
+
+    def test02_identical(self):
+        """Document and subsequent identical document"""
+        doc1 = self.create_tutorial('books01.csv')
+        self.assertTrue(doc1.action_execute())
+        self.assertEqual(len(doc1.product_tutorial_ids), 3)
+        doc2 = self.create_tutorial('books01.csv')
+        self.assertTrue(doc2.action_execute())
+        self.assertEqual(len(doc2.product_tutorial_ids), 0)
+
+    def test03_correction(self):
+        """Document and subsequent corrected document"""
+        doc1 = self.create_tutorial('books01.csv')
+        self.assertTrue(doc1.action_execute())
+        self.assertEqual(len(doc1.product_tutorial_ids), 3)
+        doc2 = self.create_tutorial('books02.csv')
+        self.assertTrue(doc2.action_execute())
+        self.assertEqual(len(doc2.product_tutorial_ids), 1)
+        self.assertEqual(doc2.product_tutorial_ids.product_id.name,
+                         "The Fifth Elephant")
