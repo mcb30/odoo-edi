c9a7edd35c599c8e6eb77334a1a3e3a8ffa29488
Michele Costa <michele.costa@unipart.io>
2018-10-01 23:41:56 +0100

    [TEST] edi_sale: Add the ability to pass extra fields in create helpers
    User-story: 1587
    Signed-off-by: Michele Costa <michele.costa@unipart.io>
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- addons/edi_sale/tests/common.py
+++ addons/edi_sale/tests/common.py
@@ -7,23 +7,27 @@ class EdiSaleCase(EdiCase):
     """Base test case for EDI sale order models"""
 
     @classmethod
-    def create_sale_line(cls, sale, product, qty):
+    def create_sale_line(cls, sale, product, qty, **kwargs):
         """Create a sale order line and attach it a sale order"""
-        return cls.env['sale.order.line'].create({
+        create_values = {
             'name': product.name,
             'order_id': sale.id,
             'product_id': product.id,
             'product_uom_qty': qty,
             'product_uom': product.uom_id.id,
             'price_unit': product.list_price,
-        })
+        }
+        create_values.update(kwargs)
+        return cls.env['sale.order.line'].create(create_values)
 
     @classmethod
-    def create_sale(cls, customer):
+    def create_sale(cls, customer, **kwargs):
         """Create a sale order"""
-        return cls.env['sale.order'].create({
+        create_values = {
             'partner_id': customer.id,
             'partner_invoice_id': customer.id,
             'partner_shipping_id': customer.id,
             'pricelist_id': cls.env.ref('product.list0').id,
-        })
+        }
+        create_values.update(kwargs)
+        return cls.env['sale.order'].create(create_values)
