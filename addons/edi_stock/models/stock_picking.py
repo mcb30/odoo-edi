c8196b4c2bc2b551a2e07db031bf3d3b08602226
Kevin Dwyer <kevin.dwyer@unipart.io>
2020-04-09 14:38:09 +0100

    [ADD] odoo-edi: Add index to StockPicking
    User-story/9881
    Task/9995
    
    Signed-off-by: Kevin Dwyer <kevin.dwyer@unipart.io>

--- addons/edi_stock/models/stock_picking.py
+++ addons/edi_stock/models/stock_picking.py
@@ -1,6 +1,6 @@
 """Trigger document"""
 
-from odoo import api, fields, models
+from odoo import api, fields, models, tools
 
 
 class StockPickingType(models.Model):
@@ -33,6 +33,19 @@ class StockPicking(models.Model):
     """Extend ``stock.picking`` for automatic EDI document creation"""
 
     _inherit = 'stock.picking'
+
+    @api.model_cr
+    def init(self):
+        """Create indexes to improve query performance."""
+        # This index improves performance of the query in pick_report_domain.
+        super().init()
+        tools.create_index(
+            self._cr,
+            "stock_picking_state_picking_type_id_edi_report_index",
+            self._table,
+            ["state", "picking_type_id", "edi_pick_report_id"]
+        )
+        return
 
     @api.multi
     def action_done(self):
