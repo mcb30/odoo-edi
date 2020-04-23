b093a2ed8ad3175d36a7c0933c5d1ca6a64fb1b7
Robert Smith <robert.smith@unipart.io>
2020-04-23 13:19:20 +0100

    [ADD] edi_sale_json, edi_sale_forward_request: Add EDI gateway and cron
    Add a gateway and cron, such that json produced by creating sale orders can be
    ingested. Also added some additional test files to ease test effort.
    
    User-story: 9767
    Signed-off-by: Robert Smith <robert.smith@unipart.io>

--- addons/edi_sale_forward_request/__manifest__.py
+++ addons/edi_sale_forward_request/__manifest__.py
@@ -1,18 +1,20 @@
 {
     "name": "Edi Sale json forward request",
-    "summary": """Adds support for json file to exiting
-Edi functionality""",
+    "summary": """Adds support for json file to existing
+    Edi functionality""",
     "author": "Unipart Digital Team",
     "license": "LGPL-3",
     "website": "https://unipart.io",
     "category": "Extra Tools",
     "version": "11.0.1.0.0",
-    "depends": ["edi_sale", "sale_management"],
+    "depends": ["edi", "edi_sale", "sale_management"],
     "external_dependencies": {"python": ["jsonschema"]},
     "data": [
         "security/ir.model.access.csv",
         "data/edi_partner_record_data.xml",
         "data/edi_sale_forward_request_data.xml",
+        "data/edi_gateway.xml",
+        "data/ir_cron.xml",
         "views/edi_partner_views.xml",
         "views/edi_sale_forward_request_views.xml",
     ],
