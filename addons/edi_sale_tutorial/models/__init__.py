64ca035d328fc660645781cda6fcfaac3dfff056
Lorenzo Cucurachi <lorenzo.cucurachi@unipart.io>
2022-07-27 16:07:43 +0100

    [REM] udes_sale_tutorials: Remove unnecessary module
    The functionality of this module lives or will live in edi_sale.
    
    User-story: x2424
    
    Signed-off-by: Lorenzo Cucurachi <lorenzo.cucurachi@unipart.io>

--- addons/edi_sale_tutorial/models/__init__.py
+++
@@ -1,4 +0,0 @@
-"""EDI sales models"""
-
-from . import edi_sale_request_tutorial
-from . import edi_sale_report_tutorial
--- addons/edi_sale_tutorial/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-"""EDI sales tests"""
-
-from . import test_sale_report_tutorial
-from . import test_sale_request_tutorial
