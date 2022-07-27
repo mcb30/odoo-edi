f5b8b400dc35829f333700582ace50aaf0efb110
Lorenzo Cucurachi <lorenzo.cucurachi@unipart.io>
2022-07-27 17:01:50 +0100

    [IMP] edi_sale: Mark tests as skipped and explain reasoning better
    These tests need some thinking.
    
    User-story: x2424
    
    Signed-off-by: Lorenzo Cucurachi <lorenzo.cucurachi@unipart.io>

--- addons/edi_sale/tests/test_fail_fast.py
+++ addons/edi_sale/tests/test_fail_fast.py
@@ -1,5 +1,5 @@
 """Tests for Order Import hardening."""
-
+import unittest
 from unittest import mock
 
 from odoo import tools
@@ -119,37 +119,40 @@ class TestFailFast(EdiSaleCase):
         self.assertEqual(Partner.search_count([('name', '=', 'Alice')]), 1)
 
     # Not Working because the hack for create_sale is not raising an exception, is returning it
-    # which is not getting picked up by the edi_synchronizer execute() method in the try and catch for the
-    # creation of target model records
+    # which is not getting picked up by the edi_synchronizer execute() method in the try and catch
+    # for the creation of target model records.
     #
-    # def test06_removes_order_if_creation_fails(self):
-    #     """Handle the case where an error occurs during order creation."""
-    #     Sale = self.env['sale.order']
-    #     create_method = Sale.create
-    #
-    #     # This is a bit hacky
-    #     def create_sale(*args, **kwargs):
-    #         cls, vals = args
-    #         if vals['origin'] == 'ORD02':
-    #             return ValidationError('Test Error')
-    #         return create_method(vals)
-    #
-    #     doc = self.create_tutorial('order05.csv', fail_fast=False)
-    #     with mock.patch.object(Sale.__class__, 'create', create=True, autospec=True, side_effect=create_sale):
-    #         self.assertTrue(doc.action_execute())
-    #     sales = doc.mapped('sale_request_tutorial_ids.sale_id')
-    #     self.assertEqual(len(sales), 2)
-    #     self.assertEqual(doc.sale_ids, sales)
-    #     self.assert_sales_in_state(sales)
-    #     sales_by_name = {x.origin: x for x in sales}
-    #     self.assertNotIn('ORD02', sales_by_name)
-    #     self.assertEqual(sales_by_name['ORD01'].partner_id.name, 'Alice')
-    #     self.assertEqual(len(sales_by_name['ORD01'].order_line), 1)
-    #     self.assertEqual(sales_by_name['ORD03'].partner_id.name, 'Carol')
-    #     self.assertEqual(len(sales_by_name['ORD03'].order_line), 1)
-    #     self.assertEqual(sales_by_name['ORD03'].order_line.product_id,
-    #                      self.cherry)
-    #     self.assertEqual(sales_by_name['ORD03'].order_line.product_uom_qty, 3)
+    # Consideration: are we trying to test a case when a whole order fails apart from incorrect details?
+    #                In that case we would need a better way to replicate a that scenario rather than hacky solutions.
+    @unittest.skip("Test is not valid at the moment")
+    def test06_removes_order_if_creation_fails(self):
+        """Handle the case where an error occurs during order creation."""
+        Sale = self.env['sale.order']
+        create_method = Sale.create
+
+        # This is a bit hacky
+        def create_sale(*args, **kwargs):
+            cls, vals = args
+            if vals['origin'] == 'ORD02':
+                return ValidationError('Test Error')
+            return create_method(vals)
+
+        doc = self.create_tutorial('order05.csv', fail_fast=False)
+        with mock.patch.object(Sale.__class__, 'create', create=True, autospec=True, side_effect=create_sale):
+            self.assertTrue(doc.action_execute())
+        sales = doc.mapped('sale_request_tutorial_ids.sale_id')
+        self.assertEqual(len(sales), 2)
+        self.assertEqual(doc.sale_ids, sales)
+        self.assert_sales_in_state(sales)
+        sales_by_name = {x.origin: x for x in sales}
+        self.assertNotIn('ORD02', sales_by_name)
+        self.assertEqual(sales_by_name['ORD01'].partner_id.name, 'Alice')
+        self.assertEqual(len(sales_by_name['ORD01'].order_line), 1)
+        self.assertEqual(sales_by_name['ORD03'].partner_id.name, 'Carol')
+        self.assertEqual(len(sales_by_name['ORD03'].order_line), 1)
+        self.assertEqual(sales_by_name['ORD03'].order_line.product_id,
+                         self.cherry)
+        self.assertEqual(sales_by_name['ORD03'].order_line.product_uom_qty, 3)
 
     def test07_reports_missing_order_line(self):
         """Test reports missing lines from otherwise valid orders."""
@@ -166,30 +169,30 @@ class TestFailFast(EdiSaleCase):
         self.assertEqual(expected_message, messages[0].body)
         self.assertEqual(SaleLineRequestRecord.search_count(error_domain), 0)
 
-    # Pretty sure the issue here is the same as referenced above with the hacky method
-    #
-    # def test08_reports_lines_from_missing_order(self):
-    #     """Test reports lines from completely invalid order."""
-    #     Sale = self.env['sale.order']
-    #     SaleRequestDocument = self.env['edi.sale.request.document']
-    #
-    #     expected_message = '<p>Missing order lines<br>ORD02\tBANANA\t2\tCannot identify Quotation "ORD02"</p>'
-    #     create_method = Sale.create
-    #
-    #     # This is a bit hacky
-    #     def create_sale(*args, **kw):
-    #         cls, vals = args
-    #         if vals['origin'] == 'ORD02':
-    #             return ValidationError('Test Error')
-    #         return create_method(vals)
-    #
-    #     doc = self.create_tutorial('order05.csv', fail_fast=False)
-    #     with mock.patch.object(Sale.__class__, 'create', create=True, autospec=True, side_effect=create_sale):
-    #         self.assertTrue(doc.action_execute())
-    #     SaleLineRequestRecord = SaleRequestDocument.browse().sale_line_request_record_model(doc)
-    #     error_domain = [('doc_id', '=', doc.id), ('error', '!=', False)]
-    #
-    #     messages = self.MailMessage.search([('body', 'like', 'Missing order lines%')])
-    #     self.assertEqual(len(messages), 1)
-    #     self.assertEqual(messages[0].body, expected_message)
-    #     self.assertEqual(SaleLineRequestRecord.search_count(error_domain), 0)
+    # Same issue here as referenced above with the hacky method
+    @unittest.skip("Test is not valid at the moment")
+    def test08_reports_lines_from_missing_order(self):
+        """Test reports lines from completely invalid order."""
+        Sale = self.env['sale.order']
+        SaleRequestDocument = self.env['edi.sale.request.document']
+
+        expected_message = '<p>Missing order lines<br>ORD02\tBANANA\t2\tCannot identify Quotation "ORD02"</p>'
+        create_method = Sale.create
+
+        # This is a bit hacky
+        def create_sale(*args, **kw):
+            cls, vals = args
+            if vals['origin'] == 'ORD02':
+                return ValidationError('Test Error')
+            return create_method(vals)
+
+        doc = self.create_tutorial('order05.csv', fail_fast=False)
+        with mock.patch.object(Sale.__class__, 'create', create=True, autospec=True, side_effect=create_sale):
+            self.assertTrue(doc.action_execute())
+        SaleLineRequestRecord = SaleRequestDocument.browse().sale_line_request_record_model(doc)
+        error_domain = [('doc_id', '=', doc.id), ('error', '!=', False)]
+
+        messages = self.MailMessage.search([('body', 'like', 'Missing order lines%')])
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].body, expected_message)
+        self.assertEqual(SaleLineRequestRecord.search_count(error_domain), 0)
